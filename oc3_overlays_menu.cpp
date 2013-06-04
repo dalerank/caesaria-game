98e5abadac3e6413ac43c9e2b8f5e95243f9b2f7
Silas <silas.normanton@gmail.com>
2013-06-04 00:50:02 +0000

    Added new English language localisation (locales/en.po) Included missed header file oc3_gettext.hpp in oc3_overlays_menu.cpp

--- oc3_overlays_menu.cpp
+++ oc3_overlays_menu.cpp
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with openCaesar3.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "oc3_gettext.hpp"
 #include "oc3_overlays_menu.hpp"
 #include "oc3_pushbutton.hpp"
 #include "oc3_event.hpp"
