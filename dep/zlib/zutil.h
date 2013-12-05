5657cdd7a85fd47aff3a5ba8227ab52c2dfcb9a2
dalerank <dalerank@mail.ru>
2013-12-05 19:42:39 +0400

    ZLIB_EXTERNAL undef

--- dep/zlib/zutil.h
+++ dep/zlib/zutil.h
@@ -13,11 +13,11 @@
 #ifndef ZUTIL_H
 #define ZUTIL_H
 
-#if ((__GNUC__-0) * 10 + __GNUC_MINOR__-0 >= 33) && !defined(NO_VIZ)
-#  define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
-#else
+//#if ((__GNUC__-0) * 10 + __GNUC_MINOR__-0 >= 33) && !defined(NO_VIZ)
+//#  define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
+//#else
 #  define ZLIB_INTERNAL
-#endif
+//#endif
 
 #include "zlib.h"
 
