a86852e72f7449487e54c077278f199ffd64aa40
dalerank <dalerank@rif.npp>
2013-12-18 13:36:41 +0400

    Fixed computer_city store capacity value

--- source/game/goodstore.cpp
+++ source/game/goodstore.cpp
@@ -1,17 +1,17 @@
-// This file is part of openCaesar3.
+// This file is part of CaesarIA.
 //
-// openCaesar3 is free software: you can redistribute it and/or modify
+// CaesarIA is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// openCaesar3 is distributed in the hope that it will be useful,
+// CaesarIA is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with openCaesar3.  If not, see <http://www.gnu.org/licenses/>.
+// along with CaesarIA.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "goodstore.hpp"
 #include "goodstore_simple.hpp"
@@ -152,6 +152,7 @@ void GoodStore::store( GoodStock &stock, const int amount)
 {
   GoodStock reservedStock;
   reservedStock.setType( stock.type() );
+  reservedStock.setCap( stock.cap() );
   reservedStock.setQty( amount );
 
   long reservationID = reserveStorage(reservedStock);
