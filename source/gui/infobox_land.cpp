64a6399c8ed4bc211b8d362ce080bcf6d9bca3b6
dalerank <dalerank@rif.npp>
2014-05-03 12:12:48 +0400

    Some fixes infoxbox land.

--- source/gui/infobox_land.cpp
+++ source/gui/infobox_land.cpp
@@ -34,52 +34,56 @@ InfoboxLand::InfoboxLand( Widget* parent, const Tile& tile )
   lbText->setTextAlignment( align::upperLeft, align::center );
   lbText->setWordwrap( true );
 
+  std::string text;
+  std::string title;
+
   if( tile.getFlag( Tile::tlTree ) )
   {
-    setTitle( _("##trees_and_forest_caption##") );
-    lbText->setText( _("##trees_and_forest_text##"));
+    title = "##trees_and_forest_caption##";
+    text = "##trees_and_forest_text##";
   } 
   else if( tile.getFlag( Tile::tlWater ) )
   {
-    setTitle( _("##water_caption##") );
-    lbText->setText( _("##water_text##"));
+    title = "##water_caption##";
+    text = "##water_text##";
   }
   else if( tile.getFlag( Tile::tlRock ) )
   {
-    setTitle( _("##rock_caption##") );
-    lbText->setText( _("##rock_text##"));
+    title = "##rock_caption##";
+    text = "##rock_text##";
   }
   else if( tile.getFlag( Tile::tlRoad ) )
   {
     if( tile.overlay()->type() == construction::plaza )
     {
-      setTitle( _("##plaza_caption##") );
-      lbText->setText( _("##plaza_text##"));
+      title = "##plaza_caption##";
+      text = "##plaza_text##";
     }
     else 
     {
-      setTitle( _("##road_caption##") );
-      lbText->setText( _("##road_text##"));
+      title = "##road_caption##";
+      text = "##road_text##";
     }
   }
   else if( tile.getFlag( Tile::tlMeadow ) )
   {
-    setTitle( _("##meadow_caption##") );
-    lbText->setText( _("##meadow_text##"));
+    title = "##meadow_caption##";
+    text = "##meadow_text##";
   }
   else 
   {
-    setTitle( _("##clear_land_caption##") );
-    lbText->setText( _("##clear_land_text##"));
+    title = "##clear_land_caption##";
+    text = "##clear_land_text##";
   }
   
   //int index = (size - tile.getJ() - 1 + border_size) * 162 + tile.getI() + border_size;
 
-  std::string text = StringHelper::format( 0xff, "Tile at: (%d,%d) ID:%04X",
+  text += StringHelper::format( 0xff, "Tile at: (%d,%d) ID:%04X",
                                            tile.i(), tile.j(),  
                                           ((short int) tile.originalImgId() ) );
   
-  lbText->setText( text );
+  setTitle( _( title ));
+  setText( _( text ) );
 }
 
 void InfoboxLand::setText( const std::string& text )
