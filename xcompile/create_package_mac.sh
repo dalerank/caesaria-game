f6b6f2ef684503876275424bc7b5fe506e8a590b
dalerank <dalerank@rif.npp>
2014-06-11 10:47:42 +0400

    Macos create package script add

---
+++ xcompile/create_package_mac.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+SF_PASSWORD="$1"
+
+cd ~/projects/caesaria
+git pull origin master
+REVISION_NUMBER=`git rev-list HEAD --count`
+
+rm -rf build
+mkdir build 
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j5
+
+cd ~/projects/caesaria-test
+
+echo "Compress package 4 macos"
+PACKAGE_NAME_MACOS=caesaria_nightly_linux_$REVISION_NUMBER.zip
+zip -r $PACKAGE_NAME_LINUX resources README.md caesaria.macos
+
+echo "Upload nigtly build macos to sf.net"
+sshpass -p $SF_PASSWORD scp $PACKAGE_NAME_MACOS dalerank@frs.sourceforge.net:/home/frs/project/opencaesar3/bin/
+
+sshpass -p $SF_PASSWORD scp caesaria.macos caesaria.updater dalerank@frs.sourceforge.net:/home/frs/project/opencaesar3/stuff/
+
+
