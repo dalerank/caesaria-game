9b5a7f74ce2a18108d1f2d10f026ef0124133aef
dalerank <dalerank@mail.ru>
2014-08-25 15:16:01 +0400

    Update linux32.cmake

--- xcompile/linux32.cmake
+++ xcompile/linux32.cmake
@@ -1,6 +1,4 @@
 set( CMAKE_SYSTEM_NAME Linux )
-set( CMAKE_SYSTEM_PROCESSOR i386 )
-#-----<configuration>-----------------------------------------------
 
 # One compiler generates either 32 bit or 64 bit code, -m32 says generate 32 bit:
 set( CMAKE_CXX_FLAGS_INIT "-m32" CACHE STRING "C++ flag which generates 32 bit code." )
