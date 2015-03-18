4d7c466df659a897d5c8026daaaa6b97cd5f1103
dalerank <dalerankn8@gmail.com>
2015-03-18 20:40:51 +0300

    code refactoring for population advisor.

--- source/gui/advisor_population_window.hpp
+++ source/gui/advisor_population_window.hpp
@@ -18,7 +18,7 @@
 #ifndef __CAESARIA_ADVISOR_POPULATION_WINDOW_H_INCLUDED__
 #define __CAESARIA_ADVISOR_POPULATION_WINDOW_H_INCLUDED__
 
-#include "window.hpp"
+#include "advisor_base_window.hpp"
 #include "core/scopedptr.hpp"
 #include "core/signals.hpp"
 #include "game/predefinitions.hpp"
@@ -29,7 +29,7 @@ namespace gui
 namespace advisorwnd
 {
 
-class Population : public Window
+class Population : public Base
 {
 public:
   Population( PlayerCityPtr city, Widget* parent, int id );
