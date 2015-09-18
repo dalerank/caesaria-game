eca30b94dfe4a791dd51c15a6f91af2f3d637494
dalerank <dalerankn8@gmail.com>
2015-09-18 21:20:25 +0300

    android fixes

--- android/android-project/src/org/caesaria/CaesarIA.java
+++ android/android-project/src/org/caesaria/CaesarIA.java
@@ -3,17 +3,14 @@ package net.dalerank.caesaria;
 import org.libsdl.app.SDLActivity;
 
 public class CaesarIA extends SDLActivity {
- 
-   protected String[] getLibraries() {
-        return new String[] {
-            "SDL2",
-            "aes",
-            "bzip2",
-            "lzma",
-            "pnggo",
-            "sdl_ttf",
-            "smk",
-            "main"
-        };
-    }
+  //load .so
+  static {
+    System.loadLibrary( "aes" );
+    System.loadLibrary( "bzip2" );
+    System.loadLibrary( "lzma" );
+    System.loadLibrary( "pnggo" );
+    System.loadLibrary( "sdl_ttf" );
+    System.loadLibrary( "smk" );
+    System.loadLibrary( "main" ); 	 	
+  }
 }
