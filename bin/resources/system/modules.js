4b8d88ec66af0fc7922555410dbc792fd4cb4030
dalerank <dalerankn8@gmail.com>
2016-02-08 20:54:32 +0300

    added script loading on start

---
+++ bin/resources/system/modules.js
@@ -0,0 +1,10 @@
+function loadModules()
+{
+  engine.log("CaesarIA: load modules started");
+
+  engine.loadModule(":/system/init_session.js");
+  engine.loadModule(":/system/init_gui.js");
+  engine.loadModule(":/system/winmission.js");
+}
+
+loadModules();
