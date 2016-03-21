0735eabe89db3775f3d80537dd0ef1b412a38556
dalerank <dalerankn8@gmail.com>
2016-03-21 20:15:05 +0300

    fixed compile error in infobox temple

--- source/gui/infobox_temple.cpp
+++ source/gui/infobox_temple.cpp
@@ -78,7 +78,7 @@ AboutTemple::AboutTemple(Widget* parent, PlayerCityPtr city, const Tile& tile )
                               goodRelation ? "goodmood" : "badmood" );
 
      Image& img = add<Image>( Point( 192, 140 ), divn->picture() );
-     img.setTooltipText( _(longDescr) ); */
+     img.setTooltipText( _(longDescr) );
   }
 
   if( !shortDesc.empty() )
