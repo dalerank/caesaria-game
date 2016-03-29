aa89b00f9fcd606802d139c060fd0e2775bce855
dalerank <dalerankn8@gmail.com>
2016-03-29 14:37:14 +0300

    Update win32-cross.cmake

--- xcompile/win32-cross.cmake
+++ xcompile/win32-cross.cmake
@@ -1,8 +1,8 @@
 # Define the environment for cross compiling from Linux to Win64
 SET(CMAKE_SYSTEM_NAME Windows) # Target system name
 SET(CMAKE_SYSTEM_VERSION 1)
-SET(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-g++)
+SET(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc-4.8)
+SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-g++-4.8)
 SET(CMAKE_RC_COMPILER /usr/bin/i686-w64-mingw32-windres)
 SET(CMAKE_RANLIB /usr/bin/i686-w64-mingw32-ranlib)
 
