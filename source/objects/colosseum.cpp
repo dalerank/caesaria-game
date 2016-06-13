6ff112d4ced033d5f0b6d509cb04b28eabb3c1ae
dalerank <dalerankn8@gmail.com>
2016-06-13 18:17:04 +0300

    improved resources

--- source/objects/colosseum.cpp
+++ source/objects/colosseum.cpp
@@ -37,147 +37,170 @@ REGISTER_CLASS_IN_OVERLAYFACTORY(object::colosseum, Colosseum)
 class Colosseum::Impl
 {
 public:
-  struct
-  {
-    DateTime glads;
-    DateTime lions;
-  } lastshow;
+    struct
+    {
+        DateTime glads;
+        DateTime lions;
+    } lastshow;
 };
 
 Colosseum::Colosseum() : EntertainmentBuilding(Service::colosseum, object::colosseum, Size(5,5) ), _d( new Impl )
 {
-  setPicture(ResourceGroup::entertainment, 36);
+    _fgPictures().resize(2);
 
-  _animation().load( ResourceGroup::entertainment, 37, 13);
-  _animation().setOffset(Point(122, 81));
-
-  _fgPictures().resize(2);
-
-  _addNecessaryWalker(walker::gladiator);
-  _addNecessaryWalker(walker::lionTamer);
+    _addNecessaryWalker(walker::gladiator);
+    _addNecessaryWalker(walker::lionTamer);
 }
 
 void Colosseum::deliverService()
 {
-  int saveWalkesNumber = walkers().size();
-  Service::Type lastSrvc = serviceType();
+    int saveWalkesNumber = walkers().size();
+    Service::Type lastSrvc = serviceType();
 
-  EntertainmentBuilding::deliverService();
+    EntertainmentBuilding::deliverService();
 
-  if( _animation().isRunning() )
-  {
-    _fgPictures().front().load( ResourceGroup::entertainment, 50 );
-    int currentWalkerNumber = walkers().size();
-    if( saveWalkesNumber != currentWalkerNumber )
+    if (_animation().isRunning())
     {
-      (lastSrvc == Service::colosseum
-        ? _d->lastshow.glads : _d->lastshow.lions) = game::Date::current();
+        _fgPictures().front().load( ResourceGroup::entertainment, 50 );
+        int currentWalkerNumber = walkers().size();
+        if (saveWalkesNumber != currentWalkerNumber)
+        {
+            (lastSrvc == Service::colosseum
+             ? _d->lastshow.glads : _d->lastshow.lions) = game::Date::current();
+        }
     }
-  }
-  else
-  {
-    _fgPictures().front() = Picture::getInvalid();
-    _fgPictures().back() = Picture::getInvalid();
-  }
+    else
+    {
+        _fgPictures().front() = Picture::getInvalid();
+        _fgPictures().back() = Picture::getInvalid();
+    }
 }
 
 Service::Type Colosseum::serviceType() const
 {
-  int lionValue = traineeValue( walker::lionTamer );
-  return lionValue > 0 ? Service::colosseum : Service::amphitheater;
+    int lionValue = traineeValue(walker::lionTamer);
+    return lionValue > 0 ? Service::colosseum : Service::amphitheater;
 }
 
 bool Colosseum::build( const city::AreaInfo& info )
 {
-  ServiceBuilding::build( info );
+    ServiceBuilding::build( info );
 
-  GladiatorSchoolList glSchools = info.city->statistic().objects.find<GladiatorSchool>( object::gladiatorSchool );
-  LionsNurseryList lionsNs = info.city->statistic().objects.find<LionsNursery>( object::lionsNursery );
+    size_t glSchools = info.city->statistic().objects.count(object::gladiatorSchool);
+    size_t lionsNs = info.city->statistic().objects.count(object::lionsNursery);
 
-  _d->lastshow.glads = game::Date::current();
-  _d->lastshow.lions = game::Date::current();
+    _d->lastshow.glads = game::Date::current();
+    _d->lastshow.lions = game::Date::current();
 
-  if( glSchools.empty() )
-  {
-    _setError( "##need_gladiator_school##" );
-  }
+    if (!glSchools)
+    {
+        _setError( "##need_gladiator_school##" );
+    }
 
-  if( lionsNs.empty() )
-  {
-    _setError( "##need_gladiator_school##" );
-  }
+    if (!lionsNs)
+    {
+        _setError( "##need_gladiator_school##" );
+    }
 
-  return true;
+    return true;
 }
 
 std::string Colosseum::troubleDesc() const
 {
-  std::string ret = EntertainmentBuilding::troubleDesc();
+    std::string ret = EntertainmentBuilding::troubleDesc();
 
-  if( !ret.empty() )
-    return ret;
+    if (!ret.empty())
+        return ret;
 
-  bool lions = isShowLionBattles();
-  bool gladiators = isShowGladiatorBattles();
-  if( !(lions && gladiators))
-  {
-    return "##trouble_colosseum_no_shows##";
-  }
+    bool lions = isShowLionBattles();
+    bool gladiators = isShowGladiatorBattles();
+    if (!(lions && gladiators))
+    {
+        return "##trouble_colosseum_no_shows##";
+    }
 
-  if( lions && gladiators )  { return "##trouble_colosseum_full_work##";  }
-  else if( lions ) { return "##colosseum_have_only_lions##"; }
-  else             { return "##trouble_colosseum_have_only_gladiatros##"; }
+    if (lions && gladiators)
+    {
+        return "##trouble_colosseum_full_work##";
+    }
+    else if (lions)
+    {
+        return "##colosseum_have_only_lions##";
+    }
+    else
+    {
+        return "##trouble_colosseum_have_only_gladiatros##";
+    }
 }
 
 bool Colosseum::isNeedGladiators() const
 {
-  GladiatorSchoolList colosseums = _city()->statistic().objects.find<GladiatorSchool>( object::gladiatorSchool );
+    GladiatorSchoolList colosseums = _city()->statistic().objects.find<GladiatorSchool>(object::gladiatorSchool);
 
-  return colosseums.empty();
+    return colosseums.empty();
 }
 
 Service::Type Colosseum::_getServiceManType() const
 {
-  auto servicemen = walkers().select<ServiceWalker>();
-  return (!servicemen.empty() ? servicemen.front()->serviceType() : Service::srvCount);
+    auto servicemen = walkers().select<ServiceWalker>();
+    return (!servicemen.empty() ? servicemen.front()->serviceType() : Service::srvCount);
 }
 
 Variant Colosseum::getProperty(const std::string & name) const
 {
-  if (name == "isNeedGladiators") return isNeedGladiators();
-  if (name == "isShowLionBattles") return isShowLionBattles();
-  if (name == "lastAnimalBoutDate") return lastAnimalBoutDate();
-  if (name == "isShowGladiatorBattles") return isShowGladiatorBattles();
-  if (name == "lastGladiatorBoutDate") return lastGladiatorBoutDate();
+    if (name == "isNeedGladiators") return isNeedGladiators();
+    if (name == "isShowLionBattles") return isShowLionBattles();
+    if (name == "lastAnimalBoutDate") return lastAnimalBoutDate();
+    if (name == "isShowGladiatorBattles") return isShowGladiatorBattles();
+    if (name == "lastGladiatorBoutDate") return lastGladiatorBoutDate();
 
-  return EntertainmentBuilding::getProperty(name);
+    return EntertainmentBuilding::getProperty(name);
 }
 
-bool Colosseum::isShowGladiatorBattles() const {  return _getServiceManType() == Service::amphitheater; }
-bool Colosseum::isShowLionBattles() const{  return _getServiceManType() == Service::colosseum; }
-int Colosseum::maxVisitors() const{ return 1500; }
-DateTime Colosseum::lastAnimalBoutDate() const { return _d->lastshow.lions; }
-DateTime Colosseum::lastGladiatorBoutDate() const { return _d->lastshow.glads; }
+bool Colosseum::isShowGladiatorBattles() const
+{
+    return _getServiceManType() == Service::amphitheater;
+}
+
+bool Colosseum::isShowLionBattles() const
+{
+    return _getServiceManType() == Service::colosseum;
+}
+
+int Colosseum::maxVisitors() const
+{
+    return 1500;
+}
+
+DateTime Colosseum::lastAnimalBoutDate() const
+{
+    return _d->lastshow.lions;
+}
+
+DateTime Colosseum::lastGladiatorBoutDate() const
+{
+    return _d->lastshow.glads;
+}
 
 void Colosseum::save(VariantMap& stream) const
 {
-  EntertainmentBuilding::save( stream );
-  VARIANT_SAVE_ANY_D( stream, _d, lastshow.glads )
-  VARIANT_SAVE_ANY_D( stream, _d, lastshow.lions )
+    EntertainmentBuilding::save( stream );
+    VARIANT_SAVE_ANY_D( stream, _d, lastshow.glads )
+    VARIANT_SAVE_ANY_D( stream, _d, lastshow.lions )
 }
 
 void Colosseum::load(const VariantMap& stream)
 {
-  EntertainmentBuilding::load( stream );
-  VARIANT_LOAD_TIME_D( _d, lastshow.glads, stream )
-  VARIANT_LOAD_TIME_D( _d, lastshow.lions, stream )
+    EntertainmentBuilding::load( stream );
+    VARIANT_LOAD_TIME_D( _d, lastshow.glads, stream )
+    VARIANT_LOAD_TIME_D( _d, lastshow.lions, stream )
 }
 
 WalkerList Colosseum::_specificWorkers() const
 {
-  WalkerList ret = walkers();
-  utils::excludeByType( ret, WalkerTypeSet(walker::lionTamer,
-                                           walker::gladiator ) );
+    WalkerList ret = walkers();
+    utils::excludeByType( ret, WalkerTypeSet(walker::lionTamer,
+                          walker::gladiator ) );
 
-  return ret;
+    return ret;
 }
